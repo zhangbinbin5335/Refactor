1dfeb3ec31daaead68ecd73d3b554f20e23e0710
zhangbinbin <zhangbinbin@akiras-iMac.local>
2017-06-22 15:02:50 +0800

    首页新闻详情head完成

---
+++ CalfTrader/MVP/HomePage/View/CTHmPgNwsDtalHdrView.h
@@ -0,0 +1,13 @@
+//
+//  CTHmPgNwsDtalHdrView.h
+//  CalfTrader
+//
+//  Created by 张彬彬 on 2017/6/21.
+//  Copyright © 2017年 上海宏鹿. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CTHmPgNwsDtalHdrView : UIView
+
+@end
---
+++ CalfTrader/MVP/HomePage/View/CTHmPgNwsDtalHdrView.m
@@ -0,0 +1,30 @@
+//
+//  CTHmPgNwsDtalHdrView.m
+//  CalfTrader
+//
+//  Created by 张彬彬 on 2017/6/21.
+//  Copyright © 2017年 上海宏鹿. All rights reserved.
+//
+
+#import "CTHmPgNwsDtalHdrView.h"
+
+@implementation CTHmPgNwsDtalHdrView
+
+#pragma mark - ♻️life cycle
+-(instancetype)initWithFrame:(CGRect)frame{
+    if (self = [super initWithFrame:frame]) {
+        
+    }
+    
+    return self;
+}
+
+#pragma mark - 🔒private
+
+#pragma mark - 🔄overwrite
+
+#pragma mark - 🚪public
+
+#pragma mark - ☸getter and setter
+
+@end
